04de0e0ebe2544a2716649d4973d605dfd8a1983
Adedayo-Data <theophilusdayo@outlook.com>
2025-12-09 22:01:00 +0100

    feat: Add Lombok's `@Getter` annotation to `BaseEntity` for automatic getter generation.

--- src/main/java/com/hng/wallet_service/models/BaseEntity.java
+++ src/main/java/com/hng/wallet_service/models/BaseEntity.java
@@ -4,11 +4,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
 
 @MappedSuperclass
+@Getter
 public abstract class BaseEntity {
 
     @Id
